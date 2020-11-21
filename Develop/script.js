7bb73c122f8da0a2e765aaf8fad66397e18ecf97
Local Al <alexander.s.woolley@gmail.com>
2020-11-20 18:47:58 -0800

    generatePassword fucntion live

--- Develop/script.js
+++ Develop/script.js
@@ -1,71 +1,105 @@
 // Assign object - potential characters
 const characters = {
-  low: 'abcdefghijklmnopqrstuvwxyz',
-  up: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  num: '0123456789',
+  low: "abcdefghijklmnopqrstuvwxyz",
+  up: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  num: "0123456789",
   sym: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
 };
 
 // Assign initializer button and text output
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("password");
+var passwordText = document.querySelector("#password");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", selectProperties) 
+generateBtn.addEventListener("click", selectProperties);
 
 // var assignment
 var length;
-var lowerCase; 
-var upperCase; 
-var numeric; 
-var special; 
-var password;
+var lowerCase;
+var upperCase;
+var numeric;
+var special;
+var password = "";
+var finalPassword = "";
 
+function generatePassword() {
+  if (lowerCase) {
+    password += characters.low;
+  }
+  if (upperCase) {
+    password += characters.up;
+  }
+  if (numeric) {
+    password += characters.num;
+  }
+  if (special) {
+    password += characters.sym;
+  }
+  console.log(password)
+}
 // Build password with prompts & confirms for properties
 function selectProperties() {
-  var length = Number(prompt("Enter desired password length"));
-  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be between 8 and 128 characters. Please enter again."));
 
-  var lowerCase = confirm("Would you like password to include 'Lower Case' characters?");
-  var upperCase = confirm("Would you like password to include 'Upper Case' characters?");
-  var numeric = confirm("Would you like password to include 'Numeric' characters?");
-  var special = confirm("Would you like password to include 'Special' characters?");
+  passwordText.textContent = "";
+  password = "";
+  finalPassword = "";
+
+   length = Number(prompt("Enter desired password length"));
+  while (isNaN(length) || length < 8 || length > 128)
+    length = Number(
+      prompt("Length must be between 8 and 128 characters. Please enter again.")
+    );
+
+   lowerCase = confirm(
+    "Would you like password to include 'Lower Case' characters?"
+  );
+   upperCase = confirm(
+    "Would you like password to include 'Upper Case' characters?"
+  );
+   numeric = confirm(
+    "Would you like password to include 'Numeric' characters?"
+  );
+   special = confirm(
+    "Would you like password to include 'Special' characters?"
+  );
 
   while (!lowerCase && !upperCase && !numeric && !special) {
     alert("You must select at least one character type!");
-    lowerCase = confirm("Would you like password to include 'Lower Case' characters?");
-    upperCase = confirm("Would you like password to include 'Upper Case' characters?");
-    numeric = confirm("Would you like password to include 'Numeric' characters?");
-    special = confirm("Would you like password to include 'Special' characters?");
+    lowerCase = confirm(
+      "Would you like password to include 'Lower Case' characters?"
+    );
+    upperCase = confirm(
+      "Would you like password to include 'Upper Case' characters?"
+    );
+    numeric = confirm(
+      "Would you like password to include 'Numeric' characters?"
+    );
+    special = confirm(
+      "Would you like password to include 'Special' characters?"
+    );
   }
-  }
-  
-// Generate once conditons met
-  if ((!isNaN(length) && !length < 8 && !length > 128) && (lowerCase || upperCase || numeric || special)) {
-  
-    function generatePassword() {
-    
-        if (lowerCase) {
-          password += characters.low
-        };
-        if (upperCase) {
-          password += charcters.up
-        };
-        if (numeric) {
-          password += charcters.num
-        };
-        if (special) {
-          password += charcters.sym
-        };
 
-        for (var i = password.length; i < length; i++) {
-          password += characters[Math.floor(Math.random() * characters.length)]    
-      }
-    }
-    
+ 
+
     //call function
-    generatePassword
+    generatePassword();
 
     // Input text
-    passwordText.textContent = password;
+ 
+    console.log(password)
+
+    for (var i =0; i < length; i++) {
+      finalPassword += password[Math.floor(Math.random() * password.length)];
+    }
+    
+    console.log(finalPassword)
+    passwordText.textContent = finalPassword;
+
+   
   }
+
+  
+   
+
+
+// Generate once conditons met
